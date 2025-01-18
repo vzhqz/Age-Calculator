a2e25b846d3ac40289acdd2c738c1f79c65e7855
vzqhz <hagebd53@gmail.com>
2025-01-18 12:22:21 +0200

    Changed the loader thingy

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-const gif = document.getElementById("loadingGif");
+const loader = document.getElementById("loader");
 const result = document.getElementById("resultText");
 const age = document.getElementById("userAge");
 
 function calculateAge() {
-    gif.style.display = "block";
+    loader.style.display = "block";
     result.textContent = "";
 
     if(age.value === "") {
         setTimeout(() => {
-            gif.style.display = "none";
+            loader.style.display = "none";
             result.innerHTML = "You can't leave the input field empty <strong>lil nigga</strong>";
         }, 3000);
     } else {
         setTimeout(() => {
-            gif.style.display = "none";
+            loader.style.display = "none";
             result.innerHTML = `Your age is <strong>${age.value}</strong>`;
         }, 3000);
 
     }
-
-    }
-
+}
